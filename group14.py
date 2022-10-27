cccbdaa6f65bf4946406c43bc7f4b4ca1ff85a23
chiphanh <huechink@gmail.com>
2022-10-27 22:03:11 +0200

    group14.py code skeleton

---
+++ group14.py
@@ -0,0 +1,113 @@
+from inspect import _void
+from pickle import POP
+import random
+from select import select
+import Reporter
+import numpy as np
+
+#TBD
+TIME_LIMIT = 100000
+K_TOURNAMENT = 5
+POP_SIZE = 20
+MUTATION_RATE = 0.4
+
+class Path:
+    def __init__(self):
+        self.cycle
+        self.fitness = -1
+    #example: cycle: [4 5 6 3 1 2 0]
+
+def randomPath(path_size: int) -> Path:
+    path = np.random.permutation(path_size)
+    return path
+
+def calculate_fitness(path: Path) -> float:
+    # update fitness value
+    pass
+
+def mutate_path(path: Path, mutation_rate: float) -> Path:
+    #
+    pass
+
+def crossover_parents(p1: Path, p2: Path) -> Path:
+    pass
+
+def mutate_population(pop: np.ndarray, mutation_rate: float):
+    mutated_pop = []
+    
+    for i in range(0, len(pop)):
+        mutatedInd = mutate_path(pop[i], mutation_rate)
+        mutated_pop.append(mutatedInd)
+    return np.array(mutated_pop)
+
+
+def initialize_population(pop_size: int, path_size: int) -> np.ndarray:
+    pop = []
+    for i in range(pop_size):
+        pop.append(randomPath(path_size))
+    return np.array(pop)
+
+def selection_k_tournament(initial_pop: np.ndarray, desired_size: int) -> np.ndarray:
+    new_pop = np.array
+    for x in range (desired_size):
+        parents = random.choices(initial_pop, k= K_TOURNAMENT)
+        parents = sorted(parents, key=lambda agent: agent.fitness, reverse=True)
+        new_pop.append(parents[0])
+    return np.array()
+
+def select_2_parents(pop: np.ndarray):
+    #return p1, p2
+    pass
+
+
+def variation(population: np.ndarray) -> None:
+    mutated_population = mutate_population(population, MUTATION_RATE)
+
+    offspring = []
+    for i in range(POP_SIZE):
+        #how to choose 2 parents?,for now it's random
+        p1, p2 = select_2_parents(mutate_population)
+        offspring.append(crossover_parents(p1, p2))
+    return np.concatenate(mutated_population, offspring)
+
+def eliminate(intermediate_pop: np.ndarray, desired_size: int) -> np.ndarray:
+    pass
+
+
+class group14:
+    def __init__(self):
+        self.reporter = Reporter.Reporter(self.__class__.__name__)
+    # The evolutionary algorithm ’s main loop
+
+    def optimize(self, filename):
+        # Read distance matrix from file .
+        file = open(filename)
+        distanceMatrix = np.loadtxt(file, delimiter=",")
+        file.close()
+        # Your code here .
+        yourConvergenceTestsHere = True
+
+        #initualize the population
+        n_city = distanceMatrix.shape[0]
+        population = initialize_population(n_city)
+
+        while (yourConvergenceTestsHere):
+            meanObjective = 0.0
+            bestObjective = 0.0
+            bestSolution = np.array([1, 2, 3, 4, 5])
+            
+            population = selection_k_tournament(population, POP_SIZE)
+            intemediate_pop = variation(population)
+            population = eliminate(intemediate_pop, POP_SIZE)
+
+            # Call the reporter with :
+            # - the mean objective function value of the population
+            # - the best objective function value of the population
+            # - a 1D numpy array in the cycle notation containing the best solution
+            # with city numbering starting from 0
+            timeLeft = self.reporter.report(
+                meanObjective, bestObjective, bestSolution)
+            if timeLeft < 0:
+                break
+        # Your code here .
+        return 0
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pandas as pd
 import numpy as np
 
-MAP = pd.read_csv(r'C:\Users\zosia\OneDrive\Pulpit\MSC Bioinf Leuven\EA\EA - project\tour50.csv')
-print(MAP)
+MAP = pd.read_csv(r'../data/tour50.csv')
 #nr of cities
-R = 49
+n_city = 49
+TIME_LIMIT = 100000
+
 
 class Path:
     def __init__(self):
         self.cycle
         self.fintess
 
+def read_csv():
+    MAP = pd.read_csv(r'../data/tour50.csv')
+    #nr of cities
+    pass
+
 def randomPath():
-    path = np.random.permutation(R)
+    path = np.random.permutation(n_city)
     return path
 
 def calculate_fitness(path: Path):
@@ -26,14 +32,21 @@ def mutate_path(path: Path):
 def cross_parents():
     pass
 
-def algorith():
-    # population list of paths
+def initialize_population():
+    pass
 
+def variation(population, mutationRate):
+    # population list of paths
     for p in population:
         population[p] = mutate_path(p)
 
-    for i in range(0, len(population)\2):
-
+    for i in range(0, len(population)/2):
+        pass
 
 
 if __name__ == '__main__':
+    read_csv()
+    population = initialize_population()
+    while (TIME_LIMIT > 0):
+        TIME_LIMIT--
+        pass
