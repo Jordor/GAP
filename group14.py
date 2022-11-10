823c6ae71c01c5bacd0e3716b7ac4968871baac7
Jordor <jordi.cg89@gmail.com>
2022-11-10 22:50:04 +0100

    started testing

--- group14.py
+++ group14.py
@@ -6,7 +6,7 @@ import numpy as np
 ########################################################################################################################
 
 TIME_LIMIT = 5 * 60  # seconds
-K_TOURNAMENT = 8  # Number of candidates in the tournament
+K_TOURNAMENT = 16  # Number of candidates in the tournament
 POP_SIZE = 400  # Population size
 MUTATION_RATE = 0.10  # Percent mutation rate 1 = 100%
 NUM_MUT = 20  # maximum number of mutations
